b66989365197da25c04f36860f4706247799f351
juanjosejjjj <juanjosejjjj@gmail.com>
2024-02-12 20:11:49 -0300

    filtros

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,181 @@ import Testimonial from "@/components/testimonial";
 import OurTeam from "@/components/our-team";
 import Contact from "@/components/contact/contact";
 
+
+const tabs = [
+  {
+    label: "Diplomados",
+    content: [
+      {
+        imgUrl: "/images/content/graduates/ecografia_abdominal.jpg",
+        title: "Ecografía Abdominal",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/graduates/neurociencia_y_educacion.jpg",
+        title: "Neurociencia y Educación",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/graduates/formacion_docente.jpg",
+        title: "Formación Docente",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/graduates/atencion_al_cliente.jpg",
+        title: "Atención al Cliente",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/graduates/programacion_web.jpg",
+        title: "Programación Web",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/graduates/asesoria_familiar.jpg",
+        title: "Asesoría Familiar",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/graduates/higienista_dental.jpg",
+        title: "Higienista Dental",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/graduates/estetica_facial_y_corporal.jpg",
+        title: "Estética Facial y Corporal",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/graduates/terapia_fisica_y_regeneradora.jpg",
+        title: "Terapia Física y Regeneradora",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/graduates/dermocosmiatria_integral.jpg",
+        title: "Dermocosmiatría Integral",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+    ],
+  },
+  {
+    label: "Cursos",
+    content: [
+      {
+        imgUrl: "/images/content/courses/asistente_administrativo.jpg",
+        title: "Asistente Administrativo",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/courses/maquillaje.jpg",
+        title: "Maquillaje",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/courses/matematicas_preuniversitarias.jpg",
+        title: "Matemáticas Preuniversitarias",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/courses/ingles_basico.jpg",
+        title: "Inglés Básico",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+    ],
+  },
+  {
+    label: "Talleres",
+    content: [
+      {
+        imgUrl: "/images/content/workshops/desarrollo_de_videojuegos.jpg",
+        title: "Desarrollo de Videjuegos",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/workshops/orientacion_vocacional.jpg",
+        title: "Orientación Vocacional",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+      {
+        imgUrl: "/images/content/workshops/auto_maquillaje.jpg",
+        title: "Auto Maquillaje",
+        description:
+          "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+      },
+    ],
+  },
+];
+
 export default function Home() {
   const [searchValue, setSearchValue] = useState("");
+  const [filteredTabs, setFilteredTabs] = useState(tabs);
+  // esta funcion handleSearch hace EXACTAMENTE lo mismo que la de abajo pero usando mucho menos codigo
+  // const handleSearch = (value: string) => {
+  //   
+  //   setSearchValue(value);
+
+  //   // Filtra las pestañas en base al valor de búsqueda
+  //   const newFilteredTabs = tabs.map((tab) => ({
+  //     ...tab,
+  //     content: tab.content.filter(({ title }) =>
+  //       title.toLowerCase().includes(value.toLowerCase())
+  //     ),
+  //   }));
+  //   console.log(newFilteredTabs)
+  //   setFilteredTabs(newFilteredTabs);
+  // };
 
   const handleSearch = (value: string) => {
-    // Here, you can access the search value when Enter is pressed
-    console.log(value);
+    // Setea el valor de búsqueda en el estado
     setSearchValue(value);
+
+    // Crea un nuevo array para almacenar las pestañas filtradas
+    const newFilteredTabs = [];
+
+    // Itera sobre las pestañas originales
+    for (const tab of tabs) {
+      // Filtra los elementos de content según el valor de búsqueda
+      const filteredContent = tab.content.filter(({ title }) =>
+        title.toLowerCase().includes(value.toLowerCase())
+      );
+
+      // Si hay elementos después del filtro, agrega la pestaña al nuevo array
+      if (filteredContent.length > 0) {
+        newFilteredTabs.push({
+          ...tab,
+          content: filteredContent,
+        });
+      }
+    }
+
+    // Actualiza el estado con las pestañas filtradas
+    setFilteredTabs(newFilteredTabs);
+
+    // Muestra el nuevo array en la consola
+    console.log(newFilteredTabs);
   };
 
   return (
     <main>
       <HeroSection />
       <SearchSection onSearch={handleSearch} />
-      <Content filter={searchValue} />
+      <Content filter={searchValue} tabs={filteredTabs} />
       <AboutUs />
       <Gallery />
       <Testimonial />
--- src/components/content/tabs.tsx
+++ src/components/content/tabs.tsx
@@ -4,37 +4,27 @@ import React, {
   MouseEvent,
   useState,
   useLayoutEffect,
-  useRef,
 } from "react";
+import { TabsArray } from './content'
 
-interface TabsProps {
-  tabs: Array<{
-    label: string;
-    content: Array<{ imgUrl: string; title: string; description: string }>;
-  }>;
+type TabsProps = {
+  tabs: TabsArray;
   filter: string;
 }
 
 const Tabs: FunctionComponent<TabsProps> = ({ tabs, filter }) => {
-  const [selectedTab, setSelectedTab] = useState(0);
+  const [selectedTab, setSelectedTab] = useState(filter);
 
   const handlerTabsFunction = (
     e: MouseEvent<HTMLButtonElement>,
-    tab: number
+    tab: string
   ) => {
     e.preventDefault();
     setSelectedTab(tab);
   };
 
-  const filterProgramsFunction = (label: string) => {
-    if (filter !== "") {
-    }
-  };
-
-  const firstBtnRef = useRef<HTMLButtonElement>(null);
-
   useLayoutEffect(() => {
-    firstBtnRef.current?.focus();
+    setSelectedTab("Diplomados")
   }, []);
 
   return (
@@ -43,16 +33,15 @@ const Tabs: FunctionComponent<TabsProps> = ({ tabs, filter }) => {
         <div className="flex justify-center mb-12 flex-row">
           {tabs.map((tab, index) => (
             <button
-              ref={index === 0 ? firstBtnRef : null}
+
               key={index}
               onClick={(e: MouseEvent<HTMLButtonElement>) =>
-                handlerTabsFunction(e, index)
+                handlerTabsFunction(e, tab.label)
               }
-              className={`flex-1 sm:w-auto w-48 py-2 sm:text-3xl text-xl text-center font-medium title-font text-gray-500 ${
-                selectedTab === index || filter === ""
-                  ? "text-white border-b-4 border-primary"
-                  : ""
-              } `}
+              className={`flex-1 sm:w-auto w-48 py-2 sm:text-3xl text-xl text-center font-medium title-font text-gray-500 ${selectedTab === tab.label || filter === ""
+                ? "text-white border-b-4 border-primary"
+                : ""
+                } `}
             >
               {tab.label}
             </button>
@@ -63,15 +52,13 @@ const Tabs: FunctionComponent<TabsProps> = ({ tabs, filter }) => {
         {tabs.map((tab, index) => (
           <div
             key={tab.label}
-            className={`${selectedTab === index ? "" : "hidden"}`}
           >
             <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-              {tab.content.map((program) => (
+
+              {selectedTab === tab.label && tab.content.map((program) => (
                 <div
                   key={program.title}
-                  className={`${
-                    filter === "" ? "" : "hidden"
-                  } p-4 md:w-1/3 sm:mb-0 mb-6`}
+                  className={`p-4 md:w-1/3 sm:mb-0 mb-6`}
                 >
                   <div className="rounded-lg h-64 overflow-hidden hover:scale-110 hover:brightness-125">
                     <img
@@ -105,6 +92,7 @@ const Tabs: FunctionComponent<TabsProps> = ({ tabs, filter }) => {
             </div>
           </div>
         ))}
+
       </div>
     </>
   );
